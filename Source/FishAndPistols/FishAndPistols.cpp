ee6f84db7da74671edc18c0655bf9f45d92c7705
singateco <singateco@gmail.com>
2023-12-21 16:43:06 +0900

    클래스 작성

---
+++ Source/FishAndPistols/FishAndPistols.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FishAndPistols.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FishAndPistols, "FishAndPistols" );
