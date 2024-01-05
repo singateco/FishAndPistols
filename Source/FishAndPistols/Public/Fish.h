7e93c9b1d110f426eed2c934b2305b361299b68f
singateco <singateco@gmail.com>
2024-01-05 11:47:07 +0900

    밸런스 패치 2

--- Source/FishAndPistols/Public/Fish.h
+++ Source/FishAndPistols/Public/Fish.h
@@ -36,7 +36,7 @@ public:
 	TSubclassOf<class AGoldDropWidgetActor> GoldDropWidgetActorClass;
 
 	UPROPERTY(EditAnywhere)
-	float ZUpwardLimit {2700};
+	float ZUpwardLimit {2500};
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 MaxHP {1};
